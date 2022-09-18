1ecc05a60624022d19410d3e8b7f0cfeb9a366c6
黄杰 <huangjie06@bilibili.com>
2022-09-18 15:40:38 +0800

    debounce and throttle

--- debounce-throttle.js
+++ debounce-throttle.js
@@ -1,13 +1,15 @@
+// 限制高频事件触发，interval内执行一次，如果期间再次触发重新计算时间
 function debounce (fn, interval) {
   let timer
   return function() {
-    clearTimeout(timer)
+    if (timer) clearTimeout(timer)
     timer = setTimeout(() => {
       fn.apply(this, arguments)
     }, interval);
   }
 }
 
+// 限制高频事件触发，interval内只会执行一次
 function throttle (fn, interval) {
   let flag = true
   return function() {
