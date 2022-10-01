c7c823ec2c0188cc87e70c8e5dc063bf43ccded2
黄杰 <841440416@qq.com>
2022-10-01 19:39:08 +0800

    Object.assign

---
+++ object.assign.js
@@ -0,0 +1,22 @@
+/**
+ * 实现Object.assign
+ * @param {*} target 
+ * @param  {...any} source 
+ * @returns 
+ */
+function _assign (target, ...source) {
+  if (target === null || target === undefined) {
+    throw new TypeError('Connot convert null or undefined to object')
+  }
+  const res = Object(target)
+  source.forEach(obj => {
+    if (obj) {
+      for(let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+          res[key] = obj[key]
+        }
+      }
+    }
+  })
+  return res
+}
