0d2bb2627259e4ffa8fd9eb5bc596c42cb42032a
黄杰 <841440416@qq.com>
2022-10-01 20:29:31 +0800

    template

---
+++ template.js
@@ -0,0 +1,18 @@
+
+function render (template, data) {
+  const reg = /\{\{(\w+)\}\}/
+  if (reg.test(template)) {
+    const name = reg.exec(template)[1]
+    template = template.replace(reg, data[name])
+    return render(template, data)
+  }
+  console.log(template);
+  return template
+}
+
+let template = '我是{{name}}，年龄{{age}}，性别{{sex}}';
+let data = {
+  name: '姓名',
+  age: 18
+}
+render(template, data); // 我是姓名，年龄18，性别undefined
